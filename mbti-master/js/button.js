a70fe5e0e31c6bd09afb86b51cd371084439f1e7
seung-u <80995345+cse-mongoose@users.noreply.github.com>
2021-05-07 16:52:05 +0900

    Add files via upload

---
+++ mbti-master/js/button.js
@@ -0,0 +1,5 @@
+button1 = document.getElementById('start_btn')
+
+button1.onclick = function () {
+  console.log("hello world");
+}
---
+++ mbti-master/js/share.js
@@ -0,0 +1,33 @@
+const url = 'https://cookielovetype.netlify.app/';
+
+function setShare(){
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '쿠키 연애유형';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/' + resultAlt + '.png';
+  const shareURL = url + 'page/result' + resultAlt + '.html';
+
+  Kakao.Link.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareDes,
+      imageUrl: shareImage,
+      link: {
+        mobileWebUrl: shareURL,
+        webUrl: shareURL
+      },
+    }, 
+    
+    buttons: [
+      {
+        title: '결과확인하기',
+        link: {
+          mobileWebUrl: shareURL,
+          webUrl: shareURL,
+        },
+      },
+    ]
+  });
+}
